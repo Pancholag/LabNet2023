ce84e6b9aa6316d2565ffb03acd8c9657c113a63
Francisco <franciscolagaresgarcia@gmail.com>
2023-03-04 00:37:43 -0300

    metodos de los ejercicios desarrollados

---
+++ EjercicioPOO2/EjercicioPOO2/IAmATeaPotException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPOO2
+{
+    [Serializable]
+    internal class IAmATeaPotException : Exception
+    {
+        private static readonly string _message = "Excepcion basada en el codigo de respuesta 418 de HTTP";
+        public IAmATeaPotException() : base(_message) { }
+        public IAmATeaPotException(String message) : base(message) { }
+        public IAmATeaPotException(String message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ EjercicioPOO2/EjercicioPOO2/Logic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPOO2
+{
+    public class Logic
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="TypedDataSetGeneratorException"></exception>
+        public static void ExceptionThrower() 
+        {
+            throw new TypedDataSetGeneratorException("Excepcion con el nombre mas largo que vi");
+        }
+
+        public static void TeaPotThrower()
+        {
+            throw new IAmATeaPotException();
+        }
+    }
+}
